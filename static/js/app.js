d19af5cc82f7a78c184384300d49c2f65f67c18a
Nicole Housekeeper <nicolehousekeeper@Nicoles-MacBook-Air.local>
2021-06-30 16:25:24 -0400

    Add .html and .css files.

--- static/js/app.js
+++ static/js/app.js
@@ -2,6 +2,7 @@
 const tableData = data;
 // reference the HTML table using d3
 var tbody = d3.select("tbody");
+
 // build data table
 function buildTable(data) {
     // clear any existing data
@@ -16,4 +17,24 @@ function buildTable(data) {
             }
         );
     });
-}
+};
+
+// filter data based on date
+function handleClick() {
+    // Grab the datetime value from the filter
+    let date = d3.select("#datetime").property("value");
+    let filteredData = tableData;
+    // Check to see if a date was entered and filter the data using the date
+    if (date) {
+        // Apply the filter to the table data to keep only the rows matching filter
+        filteredData = filteredData.filter(row => row.datetime === date);
+    };
+    // Rebuild the table using the filtered date, if a date was entered
+    buildTable(filteredData);
+};
+
+// Attach an event to list for the form button
+d3.selectAll("#filter-btn").on("click", handleClick);
+
+// Build the table when the page loads
+buildTable(tableData);
